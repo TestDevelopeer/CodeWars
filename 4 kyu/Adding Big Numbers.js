f01e901c25fc1028938d69c9a3a182de2c798c48
TestDevelopeer <testdeveloper7967@gmail.com>
2022-10-09 13:57:52 +0300

    Adding Big Numbers

---
+++ 4 kyu/Adding Big Numbers.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    a = a.split("");
+    b = b.split("");
+    let result = "";
+    let flag = 0;
+    while(a.length || b.length || flag){
+      flag += ~~a.pop() + ~~b.pop();
+      result = flag % 10 + result;
+      flag = flag > 9;
+    }
+      return result.replace(/^0+/, '');
+  }
